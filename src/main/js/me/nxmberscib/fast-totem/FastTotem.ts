2ac24424011490ff30ebff5f517636be0a857740
CibNumeritos <imcib2498.212244.2808@gmail.com>
2024-09-20 14:39:23 -0500

    chore: added PlayerManager for splitting local/global settings

--- src/main/js/me/nxmberscib/fast-totem/FastTotem.ts
+++ src/main/js/me/nxmberscib/fast-totem/FastTotem.ts
@@ -4,14 +4,19 @@ import { SwappingListener } from "./listener/SwappingListener";
 import { ResurrectionListener } from "./listener/ResurrectionListener";
 import { ConfigCommand } from "./command/ConfigCommand";
 import { FastTotemManager } from "./manager/FastTotemManager";
+import { PlayerManager } from "./manager/PlayerManager";
 
 export default class FastTotem extends TeseractPlugin {
     public static override readonly PLUGIN_ID: string = "fasttotem";
     public static readonly LOGGER: Logger = Teseract.getLogger(this.PLUGIN_ID);
     private static readonly manager: FastTotemManager = new FastTotemManager();
+    private static readonly playerManager: PlayerManager = new PlayerManager();
 
     public static getManager() {
         return this.manager;
+    }
+    public static getPlayerManager() {
+        return this.playerManager;
     }
 
     public override onLoaded() {
